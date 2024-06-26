1d5cbdc1e4f8d9a427b73f67a6c62f690de5133c
nsakalkale <sakalkalenimish2004@gmail.com>
2024-06-26 10:36:44 +0530

    commit

--- server/app.py
+++ server/app.py
@@ -62,7 +62,3 @@ def get_message():
     except Exception as e:
         app.logger.error(f"An error occurred: {str(e)}")
         return jsonify({'error': str(e)}), 500
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
